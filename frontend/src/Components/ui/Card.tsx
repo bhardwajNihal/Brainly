810c006fdac1c2fb1a3866a2b26b97a7a78060f1
Nihal Bhardwaj <bhardwajnihal00721@gmail.com>
2025-01-13 12:49:11 +0530

    done with the generic card component

--- frontend/src/Components/ui/Card.tsx
+++ frontend/src/Components/ui/Card.tsx
@@ -1,7 +1,62 @@
+import { TweetIcon } from "../../icons/tweetIcon"
+import { VideoIcon } from "../../icons/videoIcon"
+import { DocumentIcon } from "../../icons/documentIcon"
+import { ShareIcon } from "../../icons/shareIcon"
+import { DeleteIcon } from "../../icons/deleteIcon"
 
-export function Card(){
+interface cardProps{
+    title: string,
+    link?: string,
+    type: "tweet" | "youtube" | "document" | "others"
 
-    return <div className="bg-blue-400 h-56 w-40 rounded-lg ">
+}
 
+export function Card({type,title,link}:cardProps){
+
+    return <div className="h-full min-w-32 rounded-lg bg-white shadow-md px-1 py-3">
+        
+        <div className="cardHead flex justify-between items-center text-gray-500">
+
+            <div className="flex justify-center items-center">
+                <div>
+                {(type==="tweet" && <TweetIcon size="sm"/>) 
+                || (type==="youtube" && <VideoIcon size="sm"/>) 
+                || (type==="document" && <DocumentIcon size="sm"/>)}
+                </div> 
+
+                <div className="text-gray-700 w-32 text-sm font-medium ml-2 mr-3">
+                    {title}
+                </div>
+            </div>
+
+            <div className="flex justify-center items-center gap-2">
+                <div className="hover:bg-gray-300 p-1 rounded cursor-pointer">
+                    <ShareIcon size="md"/>
+                </div>
+                <div className="hover:bg-gray-300 p-1 rounded cursor-pointer">
+                    <DeleteIcon size="sm"/>
+                </div>
+            </div>
+
+        </div>
+
+        <div className="cardMain my-2 mx-1">
+
+            {(type==="youtube" &&
+                <iframe className="w-full" src={link?.replace("watch","embed").replace("?v=","/")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>)
+                
+            || (type==="tweet" &&
+                <blockquote className="twitter-tweet">
+                <a href={link?.replace("x.com","twitter.com")}></a> 
+                </blockquote>)    
+
+            }
+
+        </div>
+
+
+        <div className="cardFoot">
+
+        </div>
     </div>
 }
--- frontend/src/Components/ui/MainArea.tsx
+++ frontend/src/Components/ui/MainArea.tsx
@@ -2,29 +2,17 @@ import { Card } from "./Card"
 
 export function MainArea(){
     
-    return <div className="min-h-[calc(100vh-4em)]  w-[calc(100svw-4rem)] md:w-[calc(100svw-12rem)] bg-gray-200 flex flex-col justify-start items-center gap-4 py-4 px-8 absolute top-16 left-12 md:left-40 box-border overflow-hidden overflow-y-auto rounded-lg">
+    return <div className="min-h-[calc(100vh-4em)]  w-[calc(100svw-4rem)] md:w-[calc(100svw-12rem)] bg-gray-200 flex flex-col justify-start items-center gap-4 py-4 px-4 lg:px-8 absolute top-16 left-12 md:left-40 box-border overflow-hidden overflow-y-auto rounded-lg">
         
     <h2 className="text-xl font-semibold text-gray-600">All content</h2>
 
-    <div className="card-section w-full h-full flex gap-6 flex-wrap justify-center">
+    <div className="card-section w-full h-full flex gap-6 flex-wrap justify-start">
 
-    <Card/> 
-    <Card/> 
-    <Card/> 
-    <Card/> 
-    <Card/> 
-    <Card/> 
-    <Card/> 
-    <Card/> 
-    <Card/> 
+    <Card type={"tweet"} title={"An Important Tweet"} link="https://x.com/i/status/1878110938877820982"/> 
 
-    <Card/> 
+    <Card type={"youtube"} title={"How to code a 2048 game in Js"} link="https://www.youtube.com/watch?v=veMa7GPhrIg"/> 
 
-
-    <Card/> 
-
-
-
+    <Card type={"document"} title={"A document with something"}/> 
     </div>
         
 
